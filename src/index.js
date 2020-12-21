51498e50404e62f5d4e4be9ca359f71f5d99ef5f
Lunaticfrg0 <emilio3scano@gmail.com>
2020-12-20 21:51:54 -0400

    Added get and patch to the API + Refactored code

--- src/index.js
+++ src/index.js
@@ -2,28 +2,114 @@ const express = require("express")
 require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
+const { ObjectID } = require("mongodb")
 
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
-    user.save().then((user)=>{
+    try{
+        await user.save()
         res.status(201).send(user)
-    }).catch((error)=>{
+    } catch(error){
         res.status(400).send(error)
-    })
+    }
 })
 
-app.post('/tasks', (req, res) =>{
+app.post('/tasks', async (req, res) =>{
     const task = new Task(req.body)
-    task.save().then(()=>{
+    try{
+        await task.save()
         res.status(201).send(task)
-    }).catch((error)=>{
-        res.status(400).send(error);
+    }catch(error){
+        res.status(400).send(error)
+    }
+})
+
+app.get('/users/:id', async (req, res)=>{
+    const _id = req.params.id
+    
+     try{
+        const user = await User.findById(_id)
+        if(!user){
+            return res.status(404).send()
+         }
+        res.send(user) 
+     }catch(error){
+        res.status(500).send()
+     }
+})
+
+app.get('/users', async (req, res)=>{
+    try{
+        const users = await User.find({})
+        res.send(users)
+    }catch(error){
+        res.status(500).send()
+    }
     })
+
+app.get('/tasks', async (req, res)=>{
+
+    try{
+        const tasks = await Task.find({})
+        res.send(tasks)
+    }catch(error){
+        res.status(500).send()
+    }
+   
+})
+app.get('/tasks/:id', async(req, res)=>{
+    const _id = req.params.id
+    
+    try{
+        const task = await Task.findById(_id)
+        if(!task){
+           return res.status(404).send()
+         }
+         res.send(task)
+    }catch(error){
+        res.status(500).send()
+    }
+})
+
+app.patch('/users/:id', async (req, res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdate = ["name", "email", 'password', 'age']
+    const isValidOperation = updates.every((update)=> allowedUpdate.includes(update))
+    if(!isValidOperation){
+        return res.status(404).send({error: 'Invalid Update'})
+    }
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if(!user){
+            res.status(404).send()
+        }
+        res.send(user)
+    }catch(error){
+        res.status(400).send(error)
+    }   
+})
+
+app.patch('/tasks/:id', async(req, res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdate = ["description", "completed"]
+    const isValidOperation = updates.every((update)=> allowedUpdate.includes(update))
+    if(!isValidOperation){
+        return res.status(404).send({error: 'Invalid Update'})
+    }
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators:true})
+        if(!task){
+            res.status(404).send()
+        }
+        res.send(task)
+    }catch(error){
+        res.status(400).send(error)
+    }
 })
 
 app.listen(port, ()=>{
